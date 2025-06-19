79a4728d1c24335bcffbb7ae1cdcfa9a42ba3d1f
topertz <k.markuci@gmail.com>
2025-06-19 08:19:00 +0200

    Delete Controllers directory

--- Controllers/Product.cs
+++
@@ -1,7 +0,0 @@
-public class Product
-{
-    public int ProductID { get; set; }
-    public string? ProductName { get; set; }
-    public int ProductPrice { get; set; }
-    public string? ImageUrl { get; set; }
-}
