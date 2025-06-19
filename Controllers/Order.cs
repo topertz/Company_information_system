f3e60ad3a55c2f155b827778d6725d241e985cae
topertz <k.markuci@gmail.com>
2025-06-19 17:30:26 +0200

    Add files via upload

---
+++ Controllers/Order.cs
@@ -0,0 +1,15 @@
+public class Order
+{
+    public int OrderID { get; set; }
+    public int UserID { get; set; }
+    public string? CustomerName { get; set; }
+    public string? CustomerEmail { get; set; }
+    public string? CustomerAddress { get; set; }
+    public int HouseNumber { get; set; }
+    public string? ProductName { get; set; }
+    public int ProductID { get; set; }
+    public int Price { get; set; }
+    public int Amount { get; set; }
+    public string? Status { get; set; }
+    public DateTime OrderDate { get; set; }
+}
---
+++ Controllers/ProductSaleSummary.cs
@@ -0,0 +1,6 @@
+public class ProductSaleSummary
+{
+    public string? ProductName { get; set; }
+    public int TotalAmount { get; set; }
+    public int TotalRevenue { get; set; }
+}
