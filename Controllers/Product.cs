0957775089caf4c81a7e439fd786f82e3edd38f6
topertz <k.markuci@gmail.com>
2025-06-21 17:22:13 +0200

    Add files via upload

---
+++ Controllers/Product.cs
@@ -0,0 +1,7 @@
+public class Product
+{
+    public int ProductID { get; set; }
+    public string? ProductName { get; set; }
+    public int ProductPrice { get; set; }
+    public string? ImageUrl { get; set; }
+}
