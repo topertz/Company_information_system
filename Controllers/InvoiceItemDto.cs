290f5d225c464bb49748a7ca9e75ed08d3286cbf
topertz <k.markuci@gmail.com>
2025-06-23 09:22:22 +0200

    Delete Controllers directory

--- Controllers/InvoiceItemDto.cs
+++
@@ -1,7 +0,0 @@
-public class InvoiceItemDto
-{
-    public int ProductID { get; set; }
-    public string? ProductName { get; set; }
-    public int Quantity { get; set; }
-    public decimal UnitPrice { get; set; }
-}
